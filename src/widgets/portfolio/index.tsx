27d32dd7a873bf31fd4b6ef5d8ed0a0c3d364d9c
damirsch <damirsch.dev@gmail.com>
2025-04-19 22:08:50 +0300

    Pagination

--- src/widgets/portfolio/index.tsx
+++ src/widgets/portfolio/index.tsx
@@ -1,19 +1,39 @@
+"use client"
 import { cn } from "@/shared/lib/utils"
 import Image from "next/image"
 import s from "./index.module.scss"
 import Plus from "@/shared/ui-kit/icons/plus"
 import {
 	Carousel,
+	CarouselApi,
 	CarouselContent,
 	CarouselItem,
 	CarouselNext,
 	CarouselPrevious,
 } from "@/shared/components/ui/carousel"
+import { useEffect, useState } from "react"
 
 const Portfolio = () => {
+	const [api, setApi] = useState<CarouselApi | null>(null)
+	const [selectedIndex, setSelectedIndex] = useState(0)
+	const [scrollSnaps, setScrollSnaps] = useState<number[]>([])
+
+	useEffect(() => {
+		if (!api) return
+		const onSelect = () => {
+			setSelectedIndex(api.selectedScrollSnap())
+		}
+		setScrollSnaps(api.scrollSnapList())
+		api.on("select", onSelect)
+		onSelect()
+		return () => {
+			api.off("select", onSelect)
+		}
+	}, [api])
+
 	return (
 		<section className={cn("flex flex-col border-t border-b mb-8", s.Portfolio)}>
-			<Carousel className={s.Carousel}>
+			<Carousel setApi={setApi} className={s.Carousel}>
 				<CarouselContent className='h-[280px] 3xs:h-[400px] sm:h-[500px] xl:h-[736px] cursor-pointer'>
 					<CarouselItem>
 						<div className={cn("relative h-full", s.Image)}>
@@ -30,8 +50,22 @@ const Portfolio = () => {
 					</CarouselItem>
 					<CarouselItem>...</CarouselItem>
 				</CarouselContent>
-				<div className='-bottom-7 left-1/2 absolute flex gap-x-4 -translate-x-1/2 translate-y-full'>
+
+				<div className='-bottom-7 left-1/2 absolute flex items-center gap-x-3 -translate-x-1/2 translate-y-full'>
 					<CarouselPrevious />
+					<div className='flex gap-x-1 mt-auto'>
+						{scrollSnaps.map((_, index) => (
+							<button
+								key={index}
+								onClick={() => api?.scrollTo(index)}
+								style={{ borderColor: "var(--border-secondary)" }}
+								className={cn(
+									"size-2 rounded-full transition-all border",
+									index === selectedIndex && "bg-border-secondary"
+								)}
+							/>
+						))}
+					</div>
 					<CarouselNext />
 				</div>
 			</Carousel>
